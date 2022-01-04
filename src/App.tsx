66d794dc61f9587708bd916a7b6b8499e54aa324
Anıl Berke Sağlam <g3vxyv@gmail.com>
2022-01-04 08:20:19 +0300

    .env added, setaxiosconfig function created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import createRoutes from "./etc/routes";
+import { setAxiosConfig } from "./etc/axios";
 
 function App() {
   const routes = createRoutes();
+  setAxiosConfig()
 
   return (
     <div className='flex flex-col items-center w-screen h-screen bg-white dark:bg-gray-800 overflow-hidden'>
--- src/etc/axios.ts
+++ src/etc/axios.ts
@@ -1,1 +1,8 @@
-export {};
+import axios from 'axios';
+
+const setAxiosConfig = () => {
+  axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+  axios.defaults.headers.common['Content-Type'] = 'application/json';
+};
+
+export {setAxiosConfig};
