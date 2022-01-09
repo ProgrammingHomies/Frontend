db838eaf0b63bc4bdab91fbb4def2d27538c8399
Semih Kesgin <semihkesgin@icloud.com>
2022-01-09 17:13:45 +0300

    Update MyPostsPage.tsx

--- src/pages/MyPostsPage.tsx
+++ src/pages/MyPostsPage.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-import {MOCK_DATA} from "../../etc/constants";
+// import {MOCK_DATA} from "../../etc/constants";
 
 function MyPostsPage() {
     const navigate = useNavigate()
@@ -9,27 +9,7 @@ function MyPostsPage() {
       <>
         <Header></Header>
         <div className='container h-full flex flex-col justify-center items-center'>
-        type usePostListProps = {
-  page?: number;
-  search?: string;
-  order?: string;
-};
-
-function usePostList({ page, search, order }: usePostListProps = {}) {
-  const config = {
-    params: {
-      page,
-      search,
-      order
-    }
-  };
-
-  return useQuery(["postList", page, search, order], async () => {
-    // const { data } = await axios.get("/posts/", config);
-    return MOCK_DATA;
-  });
-}
-
+          TEST
         </div>
       </>
     );
